a2533e91cde099cd6e4550dd1ff9064a6f05d9b7
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2021-03-01 23:32:04 -0500

    stock data with details displaying

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
-import './App.css';
-import React, {useState} from 'react'
-import axios from 'axios'
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [stocks, setStocks] = useState(null)
-  const [input, setInput] = useState('')
+  const [stocks, setStocks] = useState(null);
+  const [input, setInput] = useState("");
 
-  const fetchData = async () =>  {
-    const apiURL = `https://financialmodelingprep.com/api/v3/profile/${input}?apikey=5f19ea882fea37e649bdc3230aa7ed2c`
-    
-    const response = await axios.get(apiURL)
-    setStocks(response.data)
-    console.log(response.data)
+  const fetchData = async () => {
+    const apiURL = `https://financialmodelingprep.com/api/v3/profile/${input}?apikey=5f19ea882fea37e649bdc3230aa7ed2c`;
+
+    const response = await axios.get(apiURL);
+    setStocks(response.data);
+    console.log(response.data);
     //implement error handling
-  }
-
-  
+  };
 
   return (
     <div className="App">
-    <input
-      type="text" 
-      placeholder="Type in Stock Symbol Here..." 
-      value={input}
-      onChange={event => setInput(event.target.value)}
-    ></input>
+      <input
+        type="text"
+        placeholder="Type Stock Symbol Here..."
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      ></input>
+      <button onClick={fetchData}>Fetch Stocks</button>
 
-
-    <button onClick={fetchData}>Fetch Stocks</button>
+      <div className="stocks">
+        {stocks &&
+          stocks.map((stock, index) => {
+            return (
+              <div className="stock" key={index}>
+                <h2>{stock.companyName}</h2>
+                <div className="details">
+                  <p>Price: ${stock.price}</p>
+                  <p>{stock.exchange}</p>
+                  <p>{stock.industry}</p>
+                  <p>{stock.description}</p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
