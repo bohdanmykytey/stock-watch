bc8096ba17bda93b349eaad0f587fadc0144d89c
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2021-03-01 23:10:52 -0500

    added input to search query to be able to pull any stock

--- src/App.js
+++ src/App.js
@@ -4,18 +4,28 @@ import axios from 'axios'
 
 function App() {
   const [stocks, setStocks] = useState(null)
+  const [input, setInput] = useState('')
 
   const fetchData = async () =>  {
-    const apiURL = 'https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=5f19ea882fea37e649bdc3230aa7ed2c'
+    const apiURL = `https://financialmodelingprep.com/api/v3/profile/${input}?apikey=5f19ea882fea37e649bdc3230aa7ed2c`
     
     const response = await axios.get(apiURL)
     setStocks(response.data)
     console.log(response.data)
+    //implement error handling
   }
+
+  
 
   return (
     <div className="App">
-      
+    <input
+      type="text" 
+      placeholder="Type in Stock Symbol Here..." 
+      value={input}
+      onChange={event => setInput(event.target.value)}
+    ></input>
+
 
     <button onClick={fetchData}>Fetch Stocks</button>
     </div>
